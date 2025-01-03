522dd75d8883978001df7af25489a8f17e6a2085
snehit rai <snehit@7.com>
2025-01-03 14:44:47 +0530

    version 1.2

--- obstacles.py
+++ obstacles.py
@@ -17,14 +17,16 @@ class Obstacle(Sprite):
         height_tile =self.image.get_height()
 
         #scaling the images.
-        self.new_height_tile = self.settings.screen_heigth/12        
+        self.new_height_tile = 60       
         self.scaling_factor_tile = self.new_height_tile/height_tile      
-        self.new_width_tile =self.settings.screen_width/36       
+        self.new_width_tile =20    
         self.image =pygame.transform.scale(self.image,(int(self.new_width_tile),int(self.new_height_tile)))
         
         #setting the tile
         self.rect = self.image.get_rect()#for sprite
-        self.rect.bottomleft = (self.settings.screen_width-self.settings.screen_width/16,self.settings.screen_heigth)#for sprite.
+        self.rect.bottomleft = (self.settings.screen_width-self.settings.screen_width//16,self.settings.screen_heigth)#for sprite.
+        
+        self.image_mask=pygame.mask.from_surface(self.image)
 
     def update(self):
         """Updating the position of the obstacle."""
